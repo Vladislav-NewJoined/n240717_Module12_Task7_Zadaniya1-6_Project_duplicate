a02bad4e72dba1ec31a663447250804f4ca4f3e0
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-07-06 16:20:52 +0300

    240706 Initial commit

--- src/main/java/task11_6_1/Task11_6_1.java
+++ src/main/java/task11_6_1/Task11_6_1.java
@@ -2,11 +2,20 @@ package task11_6_1;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
 import java.util.Scanner;
+
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.IOException;
 
 public class Task11_6_1 {
     public static void main(String[] args) throws IOException {
@@ -33,14 +42,56 @@ public class Task11_6_1 {
                 
                 \s""");
 
-        CloseableHttpClient httpClient = HttpClients.createDefault();
+//        CloseableHttpClient httpClient = HttpClients.createDefault();
+//
+//        HttpGet request = new HttpGet("https://api.openweathermap.org/data/2.5/weather?lat=52.27483630743035&lon=104.25449925002037&appid=aba15956c1b0894dd6c841fd1e154d45");
+//        HttpResponse response = httpClient.execute(request);
+//        Scanner sc = new Scanner(response.getEntity().getContent());
+//        System.out.println(response.getStatusLine());
+//        while (sc.hasNext()) {
+//            System.out.println(sc.nextLine());
+//        }
+//
+////        HttpPost requestPost = new HttpPost("https://api.openweathermap.org/data/2.5/weather?lat=52.27483630743035&lon=104.25449925002037&appid=aba15956c1b0894dd6c841fd1e154d45");
+////        HttpResponse response = httpClient.execute(request);
+////        Scanner sc = new Scanner(response.getEntity().getContent());
+////        System.out.println(response.getStatusLine());
+////        while (sc.hasNext()) {
+////            System.out.println(sc.nextLine());
+////        }
 
-        HttpGet request = new HttpGet("https://api.openweathermap.org/data/2.5/weather?lat=52.27483630743035&lon=104.25449925002037&appid=aba15956c1b0894dd6c841fd1e154d45");
-        HttpResponse response = httpClient.execute(request);
-        Scanner sc = new Scanner(response.getEntity().getContent());
-        System.out.println(response.getStatusLine());
-        while (sc.hasNext()) {
-            System.out.println(sc.nextLine());
+
+
+
+
+        try {
+            URL url = new URL("https://jsonplaceholder.typicode.com/posts");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+
+            // Отправляем POST данные, если необходимо
+            String postData = "key1=value1&key2=value2";
+            try (OutputStream os = connection.getOutputStream()) {
+                byte[] input = postData.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            // Получаем ответ
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // Выводим ответ в консоль
+            System.out.println(response.toString());
+
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
