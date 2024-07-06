d3ff564774343360ca292c7922c66bb06dd23e4f
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-07-06 07:22:01 +0300

    240706 Initial commit

--- src/main/java/task11_5_1/Task11_5_1.java
+++ src/main/java/task11_5_1/Task11_5_1.java
@@ -1,6 +1,7 @@
 package task11_5_1;
 
 import java.io.*;
+import java.util.Locale;
 
 // В этом фрагменте, Draft_Module11_Task5_Zadaniya1-3_part3, всё работает до видео мин 1448
 
@@ -23,15 +24,16 @@ public class Task11_5_1 {
                   Некоторые из известных типов запросов в HTTP:
                   - GET - запрос для получения данных с сервера.
                 
+                  Примеры использования:
                   \s""");
 
-//        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("src/main/java/task11_5_1/data.dat"));
-//        final Person person = new Person("Qwerty", 20, 170);
-//        out.writeObject(person);
-//
-//        ObjectInputStream input = new ObjectInputStream(new FileInputStream("src/main/java/task11_5_1/data.dat"));
-//        Person inPerson = (Person) input.readObject();
-//        System.out.println(inPerson);
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("src/main/java/task11_5_1/data.dat"));
+        final Person person = new Person("Qwerty", 20, 170);
+        out.writeObject(person);
+
+        ObjectInputStream input = new ObjectInputStream(new FileInputStream("src/main/java/task11_5_1/data.dat"));
+        Person inPerson = (Person) input.readObject();
+        System.out.println(inPerson);
 
 
 
@@ -45,29 +47,42 @@ public class Task11_5_1 {
         AuthData data = (AuthData) input2.readObject();
         System.out.println(data);
 
+
+
+        // Пробуем ещё одним способом:
+        ObjectOutputStream out3 = new ObjectOutputStream(new FileOutputStream("src/main/java/task11_5_1/data3.dat"));
+        final Person3 person3 = new Person3("Qwerty3", 42, 192);
+        final AuthData3 authData3 = new AuthData3("StrongPassword", person3);
+        out3.writeObject(authData3);
+
+//        ObjectInputStream input3 = new ObjectInputStream(new FileInputStream("src/main/java/task11_5_1/data3.dat"));
+//        AuthData3 data3 = (AuthData3) input3.readObject();
+//        System.out.println(data3);
+
     }
 }
 
-//class Person implements Serializable{
-//    final String name;
-//    final int age;
-//    final int height;
-//
-//    Person(String name, int age, int height) {
-//        this.name = name;
-//        this.age = age;
-//        this.height = height;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Person{" +
-//                "name='" + name + '\'' +
-//                ", age=" + age +
-//                ", height=" + height +
-//                '}';
-//    }
-//}
+class Person implements Serializable{
+    final String name;
+    final int age;
+    final int height;
+
+    Person(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                '}';
+    }
+}
+
 
 
 class AuthData implements Serializable{
@@ -100,4 +115,51 @@ class Person2 /*implements Serializable*/{
                 '}';
     }
 }
+
+
+
+class AuthData3 implements Externalizable{
+    static Person3 person3;
+    String password3;
+
+    AuthData3(String password3, Person3 person3) {
+        this.password3 = password3;
+        AuthData3.person3 = person3;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(person3);
+        out.writeChars(password3.toUpperCase(Locale.ROOT));
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+//        person3 = (Person3) in.readObject();
+//        password3 = in.readLine();
+    }
+}
+
+class Person3 implements Serializable{
+    final String name;
+    final int age;
+    final int height;
+
+    Person3(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Person3{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                '}';
+    }
+}
+
+
 
