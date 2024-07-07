4edb9876d92ea034b19e3a75e80de1b67d4e5338
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-07-07 06:59:11 +0300

    240707 Initial commit

---
+++ src/main/java/task11_7_1/Task11_7_1.java
@@ -0,0 +1,23 @@
+package task11_7_1;
+
+public class Task11_7_1 {
+    public static void main(String[] args) {
+        System.out.println("""
+                Задание:\s
+                Модуль 11. Протокол “HTTP”. Задание №7:\s
+                Цель задания: знакомство с Retrofit и его возможностями\s
+                    Задание:
+                    1. Что такое Retrofit?
+                    2. Приведите сценарий его  использования?
+                
+                Решение:\s""");
+
+        System.out.println("""
+                Ответ на вопрос 1, Что такое Retrofit?:
+                  Apache в контексте протокола HTTP — это веб-сервер, официально известный как Apache HTTP Server.
+                
+                Примеры использования "Apache HTTP" с использованием запросов GET и POST:
+                \s""");
+
+    }
+}
