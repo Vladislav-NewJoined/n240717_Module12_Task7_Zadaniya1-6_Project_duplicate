b555721f65b5a99ac29b19b9f84427bcdae7b2a0
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-07-08 12:17:54 +0300

    240708 Initial commit

--- src/main/java/task11_9_1/Task11_9_1.java
+++ src/main/java/task11_9_1/Task11_9_1.java
@@ -148,3 +148,29 @@ record Receiver(String id, String title) {
                 '}';
     }
 }
+
+class LocationDto {
+    private final double latitude;
+    private final double longitude;
+
+    public LocationDto(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationDto{" +
+                "latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+}
