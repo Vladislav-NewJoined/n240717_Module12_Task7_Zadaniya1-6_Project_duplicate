ccc6137a6f45dca290e51be9917de2ea94945f6a
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-07-10 15:09:37 +0300

    240710 Initial commit

--- src/main/java/task11_9_0/Task11_9_0.java
+++ src/main/java/task11_9_0/Task11_9_0.java
@@ -1,37 +1,6 @@
 package task11_9_0;
 
-import retrofit2.Call;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.Body;
-
-//import javax.ws.rs.GET;
-import java.io.IOException;
-import java.util.List;
-
-import retrofit2.http.GET;
-import retrofit2.http.POST;
-
-// Библиотека GSON добавляется отсюда: https://github.com/google/gson в виде зависимости Maven
-
-interface ReceiversService {
-    @GET("/post")
-    Call<List<Post>> getPosts();
-
-    @POST("/posts")
-    Call<Post> createUserPost(@Body Post post);
-}
-
 public class Task11_9_0 {
-
-//    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
-//
-//    public interface JsonPlaceholderAPI {
-//        @GET("/posts/1")
-//        Call<PostModel> getPost();
-//    }
-
     public static void main(String[] args) {
         System.out.println("""
                 Задание:\s
@@ -40,113 +9,14 @@ public class Task11_9_0 {
                     Задание:
                     1. В ранее созданном проекте, в папке main  создайте New Package c названием models, далее
                        класс Receiver
-                    2. В созданный класс Receiver вставляем  соответствующий код класса  из папки target из ранее
-                       созданного проекта
-                    3. В ранее созданном Package под названием models создаем класс с названием InRadiusDto, далее
-                       копируем в него соответствующий код класса из папки target проекта созданного на предыдущих уроках
-                    4. В папке main созданного ранее проекта создайте папку New Package с названием data_sources и
-                       New Package с названием domain
-                    5. В папке data_sources создайте класс ReceiverApiDataSource в него скопируйте содержимое
-                       интерфейса ReceiversService из класса Main который содержится в папке target
-                    6. В папке  domain создайте класс ReceiveService в нем сделайте
-                       import data_sources.ReceiverApiDataSource и в созданном публичном классе реализуйте
-                       получение экземпляра  receiverApiDataSource далее реализуйте метод fetch и перенесите
-                       в него содержимое из класса  Main в находящегося в папке models, подставьте в
-                       соответствующую строчку кода  receiverApiDataSource в исключении try реализуйте
-                       возврат res.body, а в исключении catch возврат null
-                    7. В классе Main находящегося в папке models создайте сервис и передайте в него ранее созданный
-                       receiverApiDataSource
-                    8. Реализуйте циклическую загрузку данных при вводе значения latitude и longitude
                 
                 Решение:\s""");
 
         System.out.println("""
                 Решение по заданию 1, В ранее созданном проекте, в папке main  создайте New Package c
                   названием models, далее класс Receiver:
-                  GSON - это библиотека для Java, разработанная Google, которая используется для преобразования
-                  объектов Java в формат JSON и наоборот. С помощью GSON можно легко осуществлять сериализацию
-                  и десериализацию Java объектов в JSON и обратно, что часто требуется при взаимодействии с
-                  веб-сервисами или при работе с базами данных.
                 
                 Пример использования "GSON":
                 \s""");
-
-//        Gson gson = new Gson();
-//        PostModel post = new PostModel();
-//        String json = gson.toJson(post);
-//        System.out.println(json);
-//        System.out.println(gson.fromJson(json,PostModel.class));
-
-
-
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://jsonplaceholder.typicode.com")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        ReceiversService service = retrofit.create(ReceiversService.class);
-        Post newPost = new Post(1, "New PostModel Title", "New PostModel Body"); // Пример создания нового поста
-        Call<Post> repos = service.createUserPost(newPost); // Пример: создание нового поста
-        try {
-            Response<Post> res = repos.execute();
-            System.out.println(res.body());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
-
-class Post {
-    private int userId;
-    private int id;
-    private String title;
-    private String body;
-
-    public Post(int userId, String title, String body) {
-        this.userId = userId;
-        this.title = title;
-        this.body = body;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    @Override
-    public String toString() {
-        return "PostModel{" +
-                "userId=" + userId +
-                ", id=" + id +
-                ", title='" + title + '\'' +
-                ", body='" + body + '\'' +
-                '}';
-    }
-}
-
-class InRadiusDto {
-    final float latitude;
-    final float longitude;
-    final int radius;
-    final int page;
-    final int pageSize;
-
-    InRadiusDto(float latitude, float longitude, int radius, int page, int pageSize) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.radius = radius;
-        this.page = page;
-        this.pageSize = pageSize;
-    }
-}
-
-record Receiver(String id, String title) {
-
-    @Override
-    public String toString() {
-        return "Receiver{" +
-                "id='" + id + '\'' +
-                ", title='" + title + '\'' +
-                '}';
     }
 }
