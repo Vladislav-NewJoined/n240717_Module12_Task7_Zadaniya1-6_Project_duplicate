eff3838b8c46b8220573904c08e0abfe342402bc
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-07-10 15:24:26 +0300

    240710 Initial commit

---
+++ src/main/java/task12_1_0/Task12_1_0.java
@@ -0,0 +1,23 @@
+package task12_1_0;
+
+public class Task12_1_0 {
+    public static void main(String[] args) {
+        System.out.println("""
+            Задание:\s
+            Модуль 12. Базы данных и Git. Задание №1:\s
+            Цель задания: формирование базового представления о Docker и его предназначении\s
+                Задание:
+                1. Что такое Docker?
+                2. Для чего он используется?
+                3. Есть и  у него альтернативы?
+            \s""");
+
+        System.out.println("""
+            Ответ на вопрос 1, Что такое Docker?:
+            \s""");
+
+        System.out.println("""
+            Примеры:\s
+            \s""");
+    }
+}
