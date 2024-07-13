b63cb7af972b935e717ac4b8dc8ef67a264cab3c
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-07-13 16:29:08 +0300

    240713 Initial commit

--- src/main/java/task12_2_1/Select.java
+++ src/main/java/task12_2_1/Select.java
@@ -5,7 +5,8 @@ import java.sql.*;
 public class Select {
     private Connection connect() {
         // SQLite connection String
-        String url = "jdbc:sqlite:some.db";
+        String url = "jdbc:sqlite:C:\\Users\\User\\AppData\\Roaming\\DBeaverData\\workspace6\\.metadata\\sample-database-sqlite-1\\Chinook.db";
+//        String url = "jdbc:sqlite:sq.db";
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url);
@@ -13,5 +14,29 @@ public class Select {
             System.out.println(e.getMessage());
         }
         return conn;
+    }
+
+    public void selectAll() {
+        String sql = "select id,name,phone from Users where name like '%Petya%';";
+//        String sql = "select * from Users";
+
+        try (Connection conn = this.connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + "\t" +
+                        rs.getString("name") + "\t" +
+                        rs.getString("phone"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Select app = new Select();
+        app.selectAll();
     }
 }
