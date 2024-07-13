fb35990766da2c6dfda64fe8982b8b6f7b25b62f
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-07-13 17:51:20 +0300

    240713 Initial commit

---
+++ src/main/java/task12_2_1/Udate.java
@@ -0,0 +1,35 @@
+package task12_2_1;
+
+import java.sql.*;
+
+public class Udate {
+    private Connection connect() {
+        // SQLite connection String
+        String url = "jdbc:sqlite:C:\\Users\\User\\AppData\\Roaming\\DBeaverData\\workspace6\\.metadata\\sample-database-sqlite-1\\Chinook.db";
+//        String url = "jdbc:sqlite:sq.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public void selectAll() {
+//        String sql = "select id,name,phone from Users where name like '%Petya%';";
+//        String sql = "select * from Users";
+
+        try (Connection conn = this.connect();
+             Statement stmt = conn.createStatement()) {
+            stmt.execute("DELETE from Users");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Udate app = new Udate();
+        app.selectAll();
+    }
+}
