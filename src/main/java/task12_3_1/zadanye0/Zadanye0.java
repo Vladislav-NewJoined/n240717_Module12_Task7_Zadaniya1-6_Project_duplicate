8462ef8923321be89f797521c775852b6464e895
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-07-16 20:24:01 +0300

    240716 Initial commit

--- src/main/java/task12_3_1/zadanye0/Zadanye0.java
+++ src/main/java/task12_3_1/zadanye0/Zadanye0.java
@@ -2,6 +2,10 @@ package task12_3_1.zadanye0;
 
 import java.sql.*;
 
+// Пользователь и пароль для MariaDB
+// CREATE USER 'root'@'localhost' IDENTIFIED BY 'rooroo1967.A';
+// mysql -u root -p rooroo1967.A
+
 public class Zadanye0 {
     public static void main(String[] args) {
         System.out.println("""
