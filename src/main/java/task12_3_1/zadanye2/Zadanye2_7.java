98d55b50e4164096b6020e1f7c43da44becde19f
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-07-22 15:01:10 +0300

    240722 commit

--- src/main/java/task12_3_1/zadanye2/Zadanye2_7.java
+++ src/main/java/task12_3_1/zadanye2/Zadanye2_7.java
@@ -19,7 +19,6 @@ public class Zadanye2_7 {
     }
 
     private static void connect() throws SQLException {
-        System.out.println("Again, hello World");
 
         Connection conn;
         Statement stmt = null; // Создаем объект Statement stmt
@@ -52,7 +51,7 @@ public class Zadanye2_7 {
 
             // Вставка данных в таблицу 'Users'
             String insertDataQuery = "insert into Users (first_name, last_name, email, phone_number, hire_date, job_id) values " +
-                    "('Stebeb', 'King', 'SKING', '515.123.4567', '1987-06-17', 'AD_PRES'), " +
+                    "('Steben', 'King', 'SKING', '515.123.4567', '1987-06-17', 'AD_PRES'), " +
                     "('Neena', 'Kochhar', 'NKOCHHAR', '515.123.4568', '1987-06-18', 'AD_VP'), " +
                     "('Lex', 'De Haan', 'LDEHAAN', '515.123.4569', '1987-06-19', 'AD_VP'), " +
                     "('Alexander', 'Hunold', 'AHUNOLD', '590.423.4567', '1987-06-20', 'ID_PROG'), " +
