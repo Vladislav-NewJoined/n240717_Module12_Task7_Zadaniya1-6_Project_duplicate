94032ad11bbce9db5ffb8200624351953a62d6bf
Vladislav-NewJoined <125866684+Vladislav-NewJoined@users.noreply.github.com>
2024-07-23 10:27:30 +0300

    240723 Initial commit

--- src/main/java/task12_3_1/zadanye1/Zadanye1.java
+++ src/main/java/task12_3_1/zadanye1/Zadanye1.java
@@ -2,6 +2,14 @@ package task12_3_1.zadanye1;
 
 import java.sql.*;
 
+// Команда в терминале для создания базы данных MMariaDB: docker run --name mysql -d -p 3306:3306 -e MYSQL_DATABASE=somedb -e MYSQL_USER=someuser -e MYSQL_PASSWORD=123 yobasystems/alpine-mariadb
+// Значения параметров для настройки соединения в DBeaver (пришли в ответ на команду в терминале: docker inspect mysql:
+// Сервер (Хост): 172.17.0.2 (нужно писать localhost вместо этого)
+// База данных: somedb (значение переменной окружения MYSQL_DATABASE)
+// Пользователь: someuser (значение переменной окружения MYSQL_USER)
+// Пароль: 123 (значение переменной окружения MYSQL_PASSWORD)
+// Драйвер: MySQL
+
 public class Zadanye1 {
     public static void main(String[] args) {
         System.out.println("""
